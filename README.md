5883eb27603f6e74d2f38e03f8884c0fc5756e16
Krzem <krzem5.dev@gmail.com>
2021-10-20 16:30:57 +0200

    Push Update 20/10/2021, 16:30:57

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python - Fast Square Root Ratio
---
+++ src/main.py
@@ -0,0 +1,39 @@
+def bsf(m):
+	o=0
+	while ((m&1)==0):
+		o+=1
+		m>>=1
+	return o
+
+
+
+def root(n,m):
+	a=n*(n+6)+1
+	b=(n+1)*4
+	for i in range(1,m):
+		t=(a*a+n*b*b)>>(i+1)
+		b=(a*b)>>i
+		a=t
+	if ((n&15)==4):
+		a>>=7
+		b>>=7
+	else:
+		while ((a&1)==0):
+			a>>=1
+			b>>=1
+		if ((n&3)==3):
+			c=a
+			d=b
+			while (d!=0):
+				t=c%d
+				c=d
+				d=t
+			a//=c
+			b//=c
+	return (a,b)
+
+
+
+for i in range(1,50):
+	a,b=root(i,5)
+	print(f"sqrt({i}) = {a}/{b} = {a/b} (Err: {a/b-i**0.5})")
